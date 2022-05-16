581c88026e84250f7504da29c20ac4f82b8ba830
YeongTakCho <andycho1120@naver.com>
2022-05-17 02:00:38 +0900

    baekjoon 11660 구간합 구하기 done

--- baekjoon/11660.c
+++ baekjoon/11660.c
@@ -17,17 +17,37 @@ int main()
         }
     }
 
+    for (int i = 1; i < N; i++)
+    {
+        graph[0][i] += graph[0][i - 1];
+        graph[i][0] += graph[i - 1][0];
+    }
+
+    for (int i = 1; i < N; i++)
+    {
+        for (int j = 1; j < N; j++)
+        {
+            graph[i][j] += graph[i - 1][j];
+            graph[i][j] += graph[i][j - 1];
+            graph[i][j] -= graph[i - 1][j - 1];
+        }
+    }
+
     for (int i = 0; i < M; i++)
     {
         v = 0;
         scanf("%d %d %d %d", &x0, &y0, &x1, &y1);
-        for (int yp = y0 - 1; yp <= y1 - 1; yp++)
-        {
-            for (int xp = x0 - 1; xp <= x1 - 1; xp++)
-            {
-                v += graph[xp][yp];
-            }
-        }
+        x0 -= 1;
+        x1 -= 1;
+        y0 -= 1;
+        y1 -= 1;
+        v = graph[x1][y1];
+        if (x0 > 0 && y0 > 0)
+            v += graph[x0 - 1][y0 - 1];
+        if (y0 > 0)
+            v -= graph[x1][y0 - 1];
+        if (x0 > 0)
+            v -= graph[x0 - 1][y1];
         printf("%d\n", v);
     }
 }
