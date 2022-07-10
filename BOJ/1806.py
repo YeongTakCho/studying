c8aa2fcac1a723942d7e760c665098c1562c3db2
YeongTakCho <andycho1120@naver.com>
2022-07-10 14:51:37 +0900

    1806 틀렸습니다

--- BOJ/1806.py
+++ BOJ/1806.py
@@ -1,29 +1,44 @@
-# two point algorithm, false case: 13 1 1 1 1 1 1 10 10 10
-
 import sys
+
 read = sys.stdin.readline
+LEFT = -1
+RIGHT = 1
 
 
 def sol(s, lst):
+    if max(lst) >= s:
+        return 1
+
+    ans = 0
     l_p = 0
     r_p = len(lst)-1
-    sum_val = sum(lst)
 
-    if sum_val < s:
-        return 0
+    while(l_p < r_p):
+        sum_val = sum(lst[l_p:r_p+1])
+        if sum_val < s:
+            return ans
+        _l_p = l_p
+        _r_p = r_p
+        before_action = 0
+        while (sum_val > s):
+            if lst[_l_p] >= lst[_r_p]:
+                sum_val -= lst[_r_p]
+                _r_p -= 1
+                before_action = RIGHT
+            else:
+                sum_val -= lst[_l_p]
+                _l_p += 1
+                before_action = LEFT
 
-    while (sum_val > s and l_p < r_p):
-        if lst[l_p] > lst[r_p]:
-            sum_val -= lst[r_p]
-            r_p -= 1
-        else:
-            sum_val -= lst[l_p]
-            l_p += 1
+        if before_action == LEFT:
+            r_p = _r_p - 1
 
-    if sum_val >= s:
-        return r_p - l_p + 1
-    if sum_val < s:
-        return r_p - l_p + 2
+        elif before_action == RIGHT:
+            l_p = _l_p + 1
+        val = _r_p - _l_p+2
+        if val < ans or ans == 0:
+            ans = val
+    return ans
 
 
 print(sol(list(map(int, read().split()))[1], list(map(int, read().split()))))
