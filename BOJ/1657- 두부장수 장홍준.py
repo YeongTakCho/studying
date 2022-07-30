bb6661e3b1e2acc429ed1c911414923ca5be57e7
YeongTakCho <andycho1120@naver.com>
2022-07-30 11:07:34 +0900

    7/30

---
+++ BOJ/1657- 두부장수 장홍준.py
@@ -0,0 +1,17 @@
+# SOLVING code for "BOJ 1657. 두부장수 장홍준"
+# - Problem link: https://www.acmicpc.net/problem/1657
+# - MY link:
+# - Used algorithm:
+import sys
+read = sys.stdin.readline
+
+
+def solution():
+    global N, M, graph
+    pass
+
+
+if __name__ == "__main__":
+    N, M = map(int, read().split())
+    graph = [list(read().rstrip()) for _ in range(N)]
+    solution()
